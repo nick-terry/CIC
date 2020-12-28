Fixing numerical issues in CEM

cosmic-master/matlab/cem.py
@@ -108,18 +108,12 @@ class CEM:
         self.bestParamsLists = []
         self.cicLists = [] 
         
-        # Some important constants which can be changed
-        # Set a threshold beyond which we consider r_x to be zero to avoid div by zero
-        if 'eps' in kwargs:
-            self.eps = kwargs['eps']
-        else:
-            self.eps = 1e-10
-        
+        # Some important constants which can be changed     
         # Set jitter used to prevent numerical issues due to zero densities
         if 'jitter' in kwargs:
             self.jitter = kwargs['jitter']
         else:
-            self.jitter = 1e-300
+            self.jitter = 0
           
         if 'numIters' in kwargs:
             self.numIters = kwargs['numIters']
@@ -327,7 +321,7 @@ class CEM:
             for j in range(params.k()):
                 densities[:,j] = stat.multivariate_normal.pdf(_X,mu[j,:],sigma[j,:],allow_singular=self.allowSingular)
             
-            densities = np.expand_dims(np.sum(_alpha*densities,axis=1),axis=1)
+            densities = np.expand_dims(np.sum(np.exp(np.log(_alpha) + np.log(densities)),axis=1),axis=1)
             
             return densities
         
@@ -442,9 +436,8 @@ class CEM:
                 raise(e)
                 
         log_alpha_q = _log_alpha + log_densities
-  
-        # Add jitter to density to prevent div by zero error
-        log_density = np.expand_dims(spc.logsumexp(log_alpha_q,axis=1),axis=1) + self.jitter
+
+        log_density = np.expand_dims(spc.logsumexp(log_alpha_q,axis=1),axis=1)
         _log_density = np.tile(log_density,(1,params.k()))
         
         gamma = np.exp(log_alpha_q - _log_density)
@@ -477,7 +470,7 @@ class CEM:
         sigma: numpy array
     
         """
-        covar_regularization = 10**-6 #Add ridge to covar matrix to prevent numerical instability
+        covar_regularization = 10**-100 #Add ridge to covar matrix to prevent numerical instability
         
         # To make this code (slightly) more readable
         X,q = np.concatenate(self.X,axis=0),self.q
@@ -486,10 +479,11 @@ class CEM:
         
         r_div_q = np.concatenate(self.Hx_WxList,axis=0)
         
-        r_div_q_gamma = r_div_q * gamma
+        #TODO: fix div by zero in log
+        r_div_q_gamma = np.exp( np.log(r_div_q) + np.log(gamma))
         
         # Compute new alpha, mu
-        alpha = np.sum(r_div_q_gamma,axis=0)/np.sum(r_div_q)
+        alpha = np.exp( np.log(np.sum(r_div_q_gamma,axis=0)) - np.log(np.sum(r_div_q)))
         
         # Check that the mixing proportions sum to 1
         try:
@@ -855,11 +849,11 @@ class CEM:
             # Get simulation result
             Hx = self.h(x)
             # Get likelihood ratio
-            q_theta = self.q(params)
-            px = self.p(x)
-            Wx = px/q_theta(x)
+            log_q_theta = self.log_q(params)
+            log_px = self.p(x) # this actually computes the log density
+            Wx = np.exp(log_px - log_q_theta(x)) #potential numerical error here. try using logs. TODO
             
-            r_x = Hx * px
+            r_x = Hx * np.exp(log_px)
             Hx_Wx = Hx * Wx
             
         except Exception as e:
@@ -934,6 +928,12 @@ class CEM:
                     
                     print('In stage: {}'.format(s))
                     print('Num k={} resets: {}'.format(k,resetCounter))
+                    
+                    # If we have too many failures, stop the program. Otherwise this can be an
+                    # endless loop
+                    if resetCounter > 100:
+                        e = Exception('Unable to solve EM after 100 tries...aborting!')
+                        raise(e)
                 
                 # If only some of them failed, just take what we've got and break
                 else:

cosmic-master/matlab/power_grid_exp.py
@@ -31,7 +31,7 @@ def p(x):
 
     """
     
-    results = stat.norm.logpdf(x,0,6)
+    results = stat.norm.logpdf(x,0,1)
     
     # Use log transform to prevent numerical issues
     log_res = np.sum(results,axis=1)
@@ -106,14 +106,14 @@ def runReplicate(seed):
     def ttfToContingency(x):
         
         # note: the scale param is what is usually called the rate param for the exp dist
-        rate = 10
+        rate = 5
         # get time to repair 1st failure
         # r = stat.expon.rvs(loc=0,scale=rate,size=x.shape[0])
-        r = 1/rate
+        rTime = 1/rate
         
         # see if the first failure is fixed before second failure
         firstTwo = np.sort(x,axis=1)[:,:2]
-        repaired = np.abs(firstTwo[:,1]-firstTwo[:,0]) > r
+        repaired = np.abs(firstTwo[:,1]-firstTwo[:,0]) > rTime
         
         contingency = np.zeros_like(x)
     

cosmic-master/matlab/simengine.py
@@ -14,7 +14,7 @@ import scipy.stats as stat
 
 class SimulationEngine:
     
-    def __init__(self,engine=None,seed=42,simulation='39-bus',matlabPrint=False):
+    def __init__(self,engine=None,seed=42,simulation='39-bus-2',matlabPrint=False):
         
         #Start a MATLAB engine for running COSMIC simulation
         if engine is None:
@@ -34,9 +34,11 @@ class SimulationEngine:
         #Define the simulation to run
         self.simFns = {'9-bus': self.eng.sim_case9_fn,
                       '39-bus': self.eng.sim_case39_fn,
-                      '2383-bus': self.eng.sim_case2383_fn}
+                      '2383-bus': self.eng.sim_case2383_fn,
+                      '39-bus-2':self.eng.sim_case39_fn2}
         self.simToNumBranches = {'9-bus':9,
                                  '39-bus':46,
+                                 '39-bus-2':46,
                                  '2383-bus':2896}
         
         try:
@@ -124,6 +126,37 @@ class SimulationEngine:
                 blackout,lostDemand = self.simFn(br1+1,br2+1,verbose, nargout=2, stdout=self.stdout)
         # If the simulation does not converge, declare a blackout
         except matlab.engine.MatlabExecutionError:
+            blackout,lostDemand = 1,0
+        return blackout,lostDemand
+    
+    def _simulate2(self,branches,verbose=False):
+        """
+        Run COSMIC simulation of N-2 contingency in MATLAB
+
+        Parameters
+        ----------
+        branches : numpy array
+            Vector containing the components that failed.
+
+        Returns
+        -------
+        blackout : boolean
+            Whether or not a blackout occurred.
+        lostDemand : float
+            Amount of demand lost due to outage.
+
+        """
+        
+        # Need to specify nargout=2 to get two results correctly
+        _branches = matlab.int8(list(branches))
+        try:
+            if self.matlabPrint:
+                blackout,lostDemand = self.simFn(_branches,verbose, nargout=2)
+            else:
+                blackout,lostDemand = self.simFn(_branches,verbose, nargout=2, stdout=self.stdout)
+        # If the simulation does not converge, declare a blackout
+        except matlab.engine.MatlabExecutionError as e:
+            print(e)
             blackout,lostDemand = 1,0
         return blackout,lostDemand
 