Fixed Cross-Entropy Calculation

cosmic-master/matlab/bayesian_regression_test.py
@@ -224,7 +224,7 @@ def runReplicate(seed,mu_prior,sigma_prior,XY,sigma):
         return samplingOracle_mu(n, mu_prior, sigma_prior)
     
     # Variance of each coordinate in initial GMM
-    sigmaSq = 3
+    sigmaSq = 1
     
     # Initial guess for GMM params
     k = 10
@@ -249,7 +249,8 @@ def runReplicate(seed,mu_prior,sigma_prior,XY,sigma):
                             log=True,
                             verbose=True,
                             covar='homogeneous',
-                            allowSingular=True)
+                            allowSingular=True,
+                            seis=True)
     procedure.run()
     
     # Estimate the failure probability
@@ -266,7 +267,7 @@ if __name__ == '__main__':
     
     dataDim = 2
     
-    numReps = 10
+    numReps = 1
     
     # Get random seeds for each replication
     seeds = np.ceil(np.random.uniform(0,99999,size=numReps)).astype(int)
@@ -275,7 +276,7 @@ if __name__ == '__main__':
     nData = 10
     
     # this is the additive noise variance which is known
-    sigma = 1
+    sigma = 3
     
     X = stat.multivariate_normal.rvs(np.zeros((dataDim-1,)),3*np.eye(dataDim-1),size=nData)
     Y = (np.sum(3*X,axis=1,keepdims=True)+1 if len(X.shape)>1 else 3*X[:,None] + 1) + stat.norm.rvs(0,sigma,size=(nData,1))

cosmic-master/matlab/bias_test_seis_p2.py
@@ -150,8 +150,7 @@ def runReplicate(seed):
                             log=True,
                             verbose=True,
                             covar='homogeneous',
-                            alpha=.1,
-                            seis=False)
+                            alpha=.1)
     procedure.run()
     
     # Estimate the failure probability
@@ -240,7 +239,7 @@ if __name__ == '__main__':
     # x = np.random.normal(10,3,size=(5,dataDim))
     # Hx = h(x)
     
-    numReps = 1
+    numReps = 2
     
     # Get random seeds for each replication
     seeds = np.ceil(np.random.uniform(0,99999,size=numReps)).astype(int)

cosmic-master/matlab/cem.py
@@ -201,6 +201,8 @@ class CEM:
         Estimate the cross-entropy from the importance sampling
         distribution defined by eta, using the estimator from equation 3.7 of
         the paper.
+        
+        See eqn 3.18
     
         Parameters
         ----------
@@ -224,9 +226,11 @@ class CEM:
         
         n = llh.shape[0]
         
-        # TODO: maybe undo this change
-        _c_bar = np.log(1/n)-spc.logsumexp(llh + np.concatenate(self.Hx_WxList))
+        # _c_bar = np.log(1/n)+spc.logsumexp(np.concatenate(self.Hx_WxList)-llh)
+        _c_bar = -np.sum(np.exp(np.concatenate(self.Hx_WxList))*llh) / n
         
+        # _c_bar = np.exp(_c_bar)
+            
         return _c_bar
     
     def rho(self):
@@ -283,13 +287,15 @@ class CEM:
     
         """
         X = np.concatenate(self.X,axis=0)
+        HxWx = np.concatenate(self.Hx_WxList)
+        n = HxWx.shape[0] - np.sum(np.isinf(HxWx))
         
         # Compute dimension of model parameter space from the number of mixtures, k
         k = params.k()
         p = X.shape[1]
         d = (k-1)+k*(p+p*(p+1)/2)
         
-        _cic = self.c_bar(params) + self.rho()*d/X.shape[0]
+        _cic = self.c_bar(params) + self.rho()*d/n
 
         return _cic
     
@@ -1205,7 +1211,8 @@ class CEM:
             if s<=1:
                 kMin = 1
             else:
-                kMin = max(1,self.paramsList[-1].k()-3)
+                # kMin = max(1,self.paramsList[-1].k()-3)
+                kMin = 1
             
             try:
                 # Run the main operations of the stage